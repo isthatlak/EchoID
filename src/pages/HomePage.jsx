4f8c17247c0b3e8fb81d68d402896be757f8562d
isthatlak <isthatlak@gmail.com>
2025-04-15 00:38:59 +0530

    yooo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,9 @@
-import { useNavigate } from 'react-router-dom';
-import AudioRecorder from '../components/AudioRecorder';
-import Navigation from '../components/Navigation';
-import { useAudio } from '../context/AudioContext';
-
 function HomePage() {
-  const navigate = useNavigate();
-  const { addIdentifiedSong } = useAudio();
-
-  const handleRecordingComplete = (recordingData) => {
-    console.log('Recording data:', recordingData);
-
-    if (recordingData.analysisResult && recordingData.analysisResult.success) {
-      const { songId, song, analysis } = recordingData.analysisResult;
-      addIdentifiedSong({ song, analysis });
-      navigate(`/analysis/${songId}`);
-    } else {
-      setTimeout(() => {
-        alert('Unable to identify song. Please try again.');
-      }, 1000);
-    }
-  };
+  console.log("HomePage rendered");
 
   return (
-    <div className="container mx-auto p-4 pb-20">
-      <Navigation />
-      <AudioRecorder onRecordingComplete={handleRecordingComplete} />
+    <div style={{ padding: '20px', color: 'green' }}>
+      <h1>Hello from HomePage</h1>
     </div>
   );
 }
