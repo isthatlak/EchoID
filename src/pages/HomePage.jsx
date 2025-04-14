c2ecc744dfdc169a3ece9a0338fe47bf28482a7b
isthatlak <isthatlak@gmail.com>
2025-04-15 00:10:52 +0530

    dooooo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,21 @@
-// HomePage.jsx
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AudioRecorder from '../components/AudioRecorder';
 import Navigation from '../components/Navigation';
 import { useAudio } from '../context/AudioContext';
 
 function HomePage() {
-  //const [isIdentifying, setIsIdentifying] = useState(false);
   const navigate = useNavigate();
   const { addIdentifiedSong } = useAudio();
 
   const handleRecordingComplete = (recordingData) => {
     console.log('Recording data:', recordingData);
-    setIsIdentifying(true);
 
     if (recordingData.analysisResult && recordingData.analysisResult.success) {
       const { songId, song, analysis } = recordingData.analysisResult;
       addIdentifiedSong({ song, analysis });
-      setIsIdentifying(false);
       navigate(`/analysis/${songId}`);
     } else {
       setTimeout(() => {
-        setIsIdentifying(false);
         alert('Unable to identify song. Please try again.');
       }, 1000);
     }
